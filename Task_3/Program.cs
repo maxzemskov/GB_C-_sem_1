e67249ebfd0e88a2212978d506914a4ef4a95d36
mzemskov <mzemskov@dimax.digital>
2022-11-23 10:48:42 +0300

    ready_task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿	{
+		Console.WriteLine("Введите число");
+		int a = int.Parse(Console.ReadLine());
+
+		Console.Clear();
+
+		if (a % 2 > 0)
+		{
+			Console.WriteLine("число нечетное");
+
+		}
+		else
+		{
+			Console.WriteLine("Число четное");
+			
+		}
+	}
