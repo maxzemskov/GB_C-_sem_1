9d3a67fa08db343201d098c1c8707f975dd86640
mzemskov <mzemskov@dimax.digital>
2022-11-23 10:20:28 +0300

    ready_project_task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿	{
+		Console.WriteLine("Введите первое число");
+		int a = int.Parse(Console.ReadLine());
+
+		Console.WriteLine("Введите второе число");
+		int b = int.Parse(Console.ReadLine());
+
+		Console.Clear();
+
+		if (a > b)
+		{
+			Console.WriteLine("max");
+			Console.WriteLine(a);
+			Console.WriteLine("min");
+			Console.WriteLine(b);
+		}
+		else
+		{
+			Console.WriteLine("max");
+			Console.WriteLine(b);
+			Console.WriteLine("min");
+			Console.WriteLine(a);
+		}
+	}
