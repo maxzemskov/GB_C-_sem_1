9d3c77571f85d05e7cfc798c43d3b52d318ab1a9
mzemskov <mzemskov@dimax.digital>
2022-11-23 11:03:33 +0300

    task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿	{
+		Console.WriteLine("Введите число");
+		int a = int.Parse(Console.ReadLine());
+		int b = 2;
+
+		Console.Clear();
+
+		while (b <= a)
+			{
+				Console.WriteLine(b);
+				b = b+2;
+			}
+		
+	}
